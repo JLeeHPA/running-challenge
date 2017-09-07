7da1190dc515a6ee0998184ce6d7fddc3bd73225
jlee <jlee@healthpolicyassociates.com>
2017-09-07 16:24:45 -0400

    add write-up for question #2 and changed run_record_params to reject certain inputs

--- app/models/run_record.rb
+++ app/models/run_record.rb
@@ -1,5 +1,4 @@
 class RunRecord < ApplicationRecord
+	# validates :date, :difficulty, :distance, :time, presence: true
+	# validates :pace, absence: true
 end
-
-# class RunRecord < ActiveRecord::Base
-	
